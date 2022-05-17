2e48ed25bac8da3052ffb1848838b3486ca93845
SangYoun <lsyun1234@naver.com>
2022-05-17 18:33:12 +0900

    small change

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -5,8 +5,8 @@ const { Users } = require('../models/Users');
 
 usersRouter.get('/', async(req, res) => {
     try {
-        const users = await Users.find({});
-        return res.send({ users });
+        const all_users = await Users.find({});
+        return res.send({ all_users });
     } catch(err) {
         console.log(err);
         return res.status(500).send({ err: err.message });
@@ -16,12 +16,7 @@ usersRouter.get('/', async(req, res) => {
 usersRouter.post('/', async (req, res) => {
     try {
         let { mem_p, mem_id, mem_pw, mem_name, mem_company, mem_type } = req.body;
-        // let _p = req.body.mem_p;
-        // let _id = req.body.mem_name;
-        // let _pw = req.body.mem_pw;
-        // let _name = req.body.mem_name;
-        // let _company = req.body.mem_company;
-        // let _type = req.body.mem_type;
+        console.log("mem_p : " + mem_p + "\n");
 
         if (!mem_p || !mem_id || !mem_pw) return res.status(400).send({ err: "key, id, password are required" });
 
