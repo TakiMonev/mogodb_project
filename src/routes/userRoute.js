c69479d1138f27090f67b4059eb1e9270a50e4be
SangYoun <lsyun1234@naver.com>
2022-05-19 19:50:47 +0900

    backup data

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -29,39 +29,6 @@ userRouter.post('/', async (req, res) => {
     }
 })
 
-userRouter.delete('/:userId', async (req, res) => {
-    try {
-        const { userId } = req.params;
-        if (!mongoose.isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
-        const user = await User.findOneAndDelete({ _id: userId })       //deleteOne도 가능
-        return res.send({ user })
-    } catch(err) {
-        console.log(err);
-        return res.status(500).send({ err: err.message });
-    }
-})
-
-userRouter.put('/:userId', async(req, res) => {
-    try {
-        const { userId } = req.params;
-        if (!mongoose.isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
-        const { age, name } = req.body;
-        if (!age && !name) return res.status(400).send({ err: "age or name is required" });
-        if (!age) return res.status(400).send({ err: "age is required" });
-        if (typeof age !== 'number') return res.status(400).send({ err: "age must be a number" });
-        if (name && typeof name.first !== 'string' && typeof name.last !== 'string') return res.status(400).send({ err: "first and last name are strings" });
-        let updateBody = {};
-        if (age) updateBody.age = age;
-        if (name) updateBody.name = name;
-
-        const user = await User.findOneAndUpdate(userId, { age, name }, { new: true });    // { age }를 { $set: { age } }로 바꿀 수 있음
-        return res.send({ user });
-    } catch(err) {
-        console.log(err);
-        return res.status(500).send({ err: err.message });
-    }
-})
-
 module.exports = {
     userRouter
 }
